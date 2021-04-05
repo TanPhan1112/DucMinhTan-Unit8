d7172dc9bd418fca8661033ea4b6badb54cf87e2
TanPhan1112 <minhtan111293@gmail.com>
2021-04-05 15:47:54 -0700

    feat : installed eslint and prettier

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-if (a) {
+var a = 1,
+    one = 1;
+
+switch (a) {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 1:         // duplicate test expression
+        break;
+    default:
+        break;
+}
+
+switch (a) {
+    case one:
+        break;
+    case 2:
+        break;
+    case one:         // duplicate test expression
+        break;
+    default:
+        break;
+}
+
+switch (a) {
+    case "1":
+        break;
+    case "2":
+        break;
+    case "1":         // duplicate test expression
+        break;
+    default:
+        break;
 }
